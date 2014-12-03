7a3eac510fd7f4d89228a684e53b5de0df88ddb8
Aritzwebdev <etxetxiki91@gmail.com>
2014-12-03 13:22:55 +0100

    mapa

--- sagardotegia.php
+++ sagardotegia.php
@@ -48,15 +48,15 @@
 	<h1><?php echo $izena ;?></h1>
 </div>
 <?php
-	/*$con=mysql_connect("localhost", "root", "zubiri");*/
-	$con = mysqli_connect(getenv('OPENSHIFT_MYSQL_DB_HOST'), getenv('OPENSHIFT_MYSQL_DB_USERNAME'), getenv('OPENSHIFT_MYSQL_DB_PASSWORD'), "", getenv('OPENSHIFT_MYSQL_DB_PORT')) or die("Error: " . mysqli_error($con));
+	$con=mysqli_connect("localhost", "root", "zubiri","txotx");
+	/*$con = mysqli_connect(getenv('OPENSHIFT_MYSQL_DB_HOST'), getenv('OPENSHIFT_MYSQL_DB_USERNAME'), getenv('OPENSHIFT_MYSQL_DB_PASSWORD'), "", getenv('OPENSHIFT_MYSQL_DB_PORT')) or die("Error: " . mysqli_error($con));*/
 
-	/*if(!$con){
+	if(!$con){
 		echo "Conexion fallida";
 		exit;
-	}*/
+	}
 
-	mysqli_select_db($con, getenv('OPENSHIFT_APP_NAME')) or die("Error: " . mysqli_error($con));
+	/*mysqli_select_db($con, getenv('OPENSHIFT_APP_NAME')) or die("Error: " . mysqli_error($con));*/
 
 	/*if(!mysql_select_db("txotx", $con)){
 		echo "Error seleccion base de datos";
