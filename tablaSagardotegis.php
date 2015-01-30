7df60ab74dbce164cfc0a5593ab5e7834624796b
lander92 <reyeslander92@gmail.com>
2015-01-30 09:38:40 +0100

    tabla sagardotegis

---
+++ tablaSagardotegis.php
@@ -0,0 +1,43 @@
+<div class="greenBox">
+	<div class="myTable">
+		<table class="table-bordered table-striped table-condensed flip-content">
+		<form action="borrarSagardotegi.php" method="post">
+			<thead class="flip-content">
+				<tr>
+					<th>Borrar</th>
+					<th>Id</th>
+					<th>Izena</th>
+					<th>Herria</th>
+					<th>Deskribapena</th>
+					<th>Probintzia</th>
+					<th>Telefonoa</th>
+					<th>Email</th>
+					<th>Web</th>
+					<th>Balorazioa</th>
+					<th>Lat</th>
+					<th>Lng</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php 
+	include_once 'conectar.php';
+	$conexion=conectar();
+	// Seleccionar todos los registros 
+	$consulta = "SELECT * FROM sagardotegiak";
+	$result=mysqli_query($conexion, $consulta);
+	while ($registro = mysqli_fetch_row($result)){
+		echo "<tr>";
+		echo "<td><input type='checkbox' name='sagardotegiak[]' value=$registro[0]/></td>";
+		foreach($registro  as $valor){
+			echo "<td>",$valor,"</td>";
+ 		}
+	}
+	mysqli_close($conexion);
+?>
+				<tr><td colspan=12 align=center><input type="submit" name="borrar" id="btnBorrar" value="Borrar"></td></tr>
+			</tbody>
+			</form>
+		</table>
+	</div>
+
+</div>	
