a5ec1d464d4aeefe1e9551f81be1c746bd136a2e
lander92 <reyeslander92@gmail.com>
2015-01-30 09:36:18 +0100

    mejorado

--- borrarUsuario.php
+++ borrarUsuario.php
@@ -1,28 +1,20 @@
-<?php
-include 'conectar.php';
-$con=conectar();
 
-if(isset($_POST['borrarUsuario'])){
-	if(!empty($_POST['erabiltzaile'])){
-		$erabiltzailea=$_POST['erabiltzaile'];
-		$sql="SELECT * FROM erabiltzaileak WHERE erabiltzailea='$erabiltzailea'";
-		$result = mysqli_num_rows(mysqli_query($con, $sql));
-		if($result==0){
-		    echo "<script type='text/javascript'>alert('El usuario $erabiltzailea no existe');location.href = 'admin.php';</script>";
-		}else{
-		  	$sql="DELETE FROM erabiltzaileak WHERE erabiltzailea='$erabiltzailea'";
-			$result=mysqli_query($con, $sql);
-			if($result){
-				echo "<script type='text/javascript'>alert('$erabiltzailea erabiltzailea ezabatu da');location.href = 'admin.php';</script>";
-			}else{
-				echo "<script type='text/javascript'>alert('Ezin da erabiltzailea ezabatu');location.href = 'admin.php';</script>";
-			}
-		}
-		
+<?php 
+	if(isset($_POST['borrar'])){
+		include 'conectar.php';
+		$conexion=conectar();
+		$usuarios = $_POST['usuarios'];
+		foreach ($usuarios as $indice => $valor) {
+			$valor=ereg_replace("[^A-Za-z0-9]", "", $valor);
+		    $consulta = "DELETE FROM erabiltzaileak 
+		        WHERE iderabiltzaileak=$valor";
+		   	if(mysqli_query($conexion, $consulta)){
+		   		echo "<script type='text/javascript'>location.href = 'admin.php';</script>";
+		   	}else{
+		   		echo "<script type='text/javascript'>alert('Ezin da sagardotegia ezabatu');location.href = 'admin.php';</script>";
+		   	}
+		} 	 
 	}else{
-		echo "Sartu erabiltzailea";
-	}
-}else{
-	header("Location:admin.php");
-}
+		header("Location:admin.php");
+	}	
 ?>
