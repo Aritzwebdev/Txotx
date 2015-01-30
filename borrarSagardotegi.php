205d1805d86871c6301a742161c1d9e18bbe6fb7
lander92 <reyeslander92@gmail.com>
2015-01-30 09:35:45 +0100

    mejorado

--- borrarSagardotegi.php
+++ borrarSagardotegi.php
@@ -1,28 +1,19 @@
-<?php
-include 'conectar.php';
-$con=conectar();
-
-if(isset($_POST['borrarSagardotegi'])){
-	if(!empty($_POST['sagardotegi'])){
-		$sagardotegi=$_POST['sagardotegi'];
-		$sql="SELECT * FROM sagardotegiak WHERE izena='$sagardotegi'";
-		$result = mysqli_num_rows(mysqli_query($con, $sql));
-		if($result==0){
-		    echo "<script type='text/javascript'>alert('La sidreria $sagardotegi no existe');location.href = 'admin.php';</script>";
-		}else{
-		  	$sql="DELETE FROM sagardotegiak WHERE izena='$sagardotegi'";
-			$result=mysqli_query($con, $sql);
-			if($result){
-				echo "<script type='text/javascript'>alert('$sagardotegi sagardotegia ezabatu da');location.href = 'admin.php';</script>";
-			}else{
-				echo "<script type='text/javascript'>alert('Ezin da sagardotegia ezabatu');location.href = 'admin.php';</script>";
-			}
-		}
-		
+<?php 
+	if(isset($_POST['borrar'])){
+		include 'conectar.php';
+		$conexion=conectar();
+		$sagardotegis = $_POST['sagardotegiak'];
+		foreach ($sagardotegis as $indice => $valor) {
+			$valor=ereg_replace("[^A-Za-z0-9]", "", $valor);
+		    $consulta = "DELETE FROM sagardotegiak 
+		        WHERE idsagardotegiak=$valor";
+		   	if(mysqli_query($conexion, $consulta)){
+		   		echo "<script type='text/javascript'>location.href = 'admin.php';</script>";
+		   	}else{
+		   		echo "<script type='text/javascript'>alert('Ezin da sagardotegia ezabatu');location.href = 'admin.php';</script>";
+		   	}
+		} 
 	}else{
-		echo "Sartu sagardotegiaren izena";
-	}
-}else{
-	header("Location:admin.php");
-}
+		header("Location:admin.php");
+	}	
 ?>
