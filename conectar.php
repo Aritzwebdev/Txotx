eb6fb55cafe139029363b52924438341550af247
Aritzwebdev <etxetxiki91@gmail.com>
2015-02-05 11:05:23 +0100

    jajf

--- conectar.php
+++ conectar.php
@@ -1,18 +1,19 @@
 <?php
 if(!getenv('OPENSHIFT_MYSQL_DB_HOST')){
 	function conectar(){
-		$con=mysqli_connect("localhost", "root", "zubiri","txotx") or die(mysqli_error());
+		$con=mysqli_connect("localhost", "root", "zubiri") or die(mysqli_error());
+		mysqli_select_db("txotx");
 		return $con;
 	}
 }else{
 	$server=getenv('OPENSHIFT_MYSQL_DB_HOST');
-	$db=getenv('OPENSHIFT_APP_NAME');
+	//$db=getenv('OPENSHIFT_APP_NAME');
 	$user=getenv('OPENSHIFT_MYSQL_DB_USERNAME');
 	$password=getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
 	
 	function conectar(){
 		$con=mysqli_connect($server,$user,$password,"") or die(mysqli_error());
-		mysql_select_db($db);
+		mysqli_select_db("txotx");
 		return $con;
 	}
 }
